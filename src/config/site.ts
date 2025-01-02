d73105326065bbc1773b33ee09d7045c2ece2f6f
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2025-01-01 23:57:14 -0500

    feat: dynamic head

---
+++ src/config/site.ts
@@ -0,0 +1,30 @@
+import type {Site, Metadata, Socials } from "@/types";
+
+export const SITE: Site = {
+  NAME: "Chris Nowicki",
+  EMAIL: "chris@chrisnowicki.dev",
+}
+
+export const HOME: Metadata = {
+  TITLE: "Home",
+  DESCRIPTION: "Full-Stack Developer & Technology Nerd."
+}
+
+export const SOCIALS: Socials[] = [
+  {
+    NAME: 'Linkedin',
+    URL: 'https://www.linkedin.com/in/chris-nowicki/',
+  },
+  {
+    NAME: 'GitHub',
+    URL: 'https://github.com/chris-nowicki',
+  },
+  {
+    NAME: 'Twitter',
+    URL: 'https://twitter.com/iamwix',
+  },
+  {
+    NAME: 'Twitch',
+    URL: 'https://www.twitch.tv/chriswix',
+  },
+]
