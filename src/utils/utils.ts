bfd526cdf09674b157d061dd5949c9b488a840bf
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2025-01-06 16:29:16 -0500

    feat: sort posts by year on blog page

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,27 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import type { CollectionEntry } from 'astro:content'
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
+}
+
+export function groupEntriesByYear<T extends 'blog'>(
+  entries: CollectionEntry<T>[]
+): Record<number, CollectionEntry<T>[]> {
+  return entries.reduce(
+    (acc, entry) => {
+      const year = new Date(entry.data.date).getFullYear()
+      if (!acc[year]) {
+        acc[year] = []
+      }
+      acc[year].push(entry)
+      return acc
+    },
+    {} as Record<number, CollectionEntry<T>[]>
+  )
+}
+
+export function getSortedYears(posts: Record<string, any[]>) {
+  return Object.keys(posts).sort((a, b) => parseInt(b) - parseInt(a))
 }
