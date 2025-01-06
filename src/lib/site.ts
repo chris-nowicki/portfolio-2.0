89578e1200edac3aa5b646dc607e4d313655a152
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2025-01-06 10:53:16 -0500

    feat: view transitions between pages

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -11,3 +11,24 @@ export const HOME: Metadata = {
   TITLE: 'Home',
   DESCRIPTION: 'Full-Stack Developer & Technology Nerd.',
 }
+
+export const NOW: Metadata = {
+  TITLE: 'Now',
+  DESCRIPTION: 'What I am doing now.',
+}
+
+export const BLOG: Metadata = {
+  TITLE: 'Blog',
+  DESCRIPTION:
+    'Checkout my latest articles on all things tech, productivity, and web development!',
+}
+
+export const SPEAKING: Metadata = {
+  TITLE: 'Speaking',
+  DESCRIPTION: 'Upcoming and past speaking engagements.',
+}
+
+export const USES: Metadata = { 
+  TITLE: 'Uses',
+  DESCRIPTION: 'Tools, software, and hardware I use in my day-to-day life.'
+}
