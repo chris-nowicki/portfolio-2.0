5797dd66d90c850607476071b4f00823a2aaf22f
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2025-02-08 15:59:03 -0500

    fix: uses button cursor as pointer

--- src/components/Svelte/UsesFilterButton.svelte
+++ src/components/Svelte/UsesFilterButton.svelte
@@ -6,7 +6,7 @@
 
 <button
   class={cn(
-    'rounded-lg border px-4 py-2 text-sm transition-colors duration-200 ease-in-out',
+    'rounded-lg border px-4 py-2 text-sm transition-colors duration-200 ease-in-out cursor-pointer',
     {
       'bg-primary text-white': active,
       'text-primary hover:bg-primary hover:text-white': !active,
