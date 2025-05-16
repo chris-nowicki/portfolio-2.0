66ec4bb77a74836254a414cc37e747a6c24773f0
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2025-05-15 21:48:09 -0400

    update vite config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,4 +3,7 @@ import { getViteConfig } from 'astro/config'
 export default getViteConfig({
   /* for example, use global to avoid globals imports (describe, test, expect): */
   // globals: true,
+    preview: {
+    allowedHosts: ["selfhost.chrisnowicki.dev"]
+  }
 })
